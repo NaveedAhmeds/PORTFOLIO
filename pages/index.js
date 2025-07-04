e678f8e5efb42f65a4b3c58321c55282814e72d6
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-07-04 15:50:03 -0400

    Added comments

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
 		<>
 			<Navbar />
 			<main className={styles.main}>
+				{/* Reorientation required */}
 				<Hero />
 				<ExperienceSection />
 				<Projects />
